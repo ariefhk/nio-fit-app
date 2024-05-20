b25711ffb262ab60e1498884155b28279c804627
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2024-05-20 20:36:24 +0700

    feat:home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,28 @@
 import SectionWrapper from "@/components/common/section-wrapper"
+import { Input } from "@/components/ui/input"
 import AppLayout from "@/layouts/app-layout"
 
 export default function HomePage() {
   return (
     <AppLayout>
-      <SectionWrapper className="max-w-screen-lg">
-        <h1>Test</h1>
+      <SectionWrapper className="max-w-[1200px] space-y-12">
+        <div className=" pt-[70px] flex items-center">
+          <div className="w-max space-y-12">
+            <h1 className="text-[60px] leading-[70px] font-semibold">
+              Pilihan <span className="text-color-1">Artikel</span> Hari ini{" "}
+              <br /> Untuk Anda
+            </h1>
+            <Input placeholder="Cari Artikel" />
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-between items-center ">
+            <h1 className="text-[24px] font-bold leading-[44px]">Kecanduan</h1>
+            <p className="text-color-1 text-[20px] font-semibold leading-[28px]">
+              Lihat Semua
+            </p>
+          </div>
+        </div>
       </SectionWrapper>
     </AppLayout>
   )
